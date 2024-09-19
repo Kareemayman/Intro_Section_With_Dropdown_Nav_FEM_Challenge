bba1d33b5ed74a7b61e9379ccca2882a965c5de4
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-09-19 14:12:11 +0300

    Created & Finished Mobile Menu Navigation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Desktop Navigation
 featuresMenu = document.querySelector(".features .dropdown-menu")
 featuresArrowDown = document.querySelector(".features i:first-of-type")
 featuresArrowUp = document.querySelector(".features i:last-of-type")
@@ -52,5 +53,79 @@ document.addEventListener("click", (e) => {
 
     companyParag.classList.toggle("black")
     companyArrowUp.classList.toggle("black")
+  }
+})
+
+// Mobile Menu
+const mobileMenu = document.querySelector(".mobile-menu")
+const mobileFeaturesMenu = document.querySelector(
+  ".mobile-menu .features.dropdown-menu"
+)
+const mobileFeaturesArrowDown = document.querySelector(
+  ".mobile-menu .features i:first-of-type"
+)
+const mobileFeaturesArrowUp = document.querySelector(
+  ".mobile-menu .features i:last-of-type"
+)
+const mobileFeaturesParag = document.querySelector(".mobile-menu .features p")
+const mobileCompanyMenu = document.querySelector(
+  ".mobile-menu .company.dropdown-menu"
+)
+const mobileCompanyArrowDown = document.querySelector(
+  ".mobile-menu .company i:first-of-type"
+)
+const mobileCompanyArrowUp = document.querySelector(
+  ".mobile-menu .company i:last-of-type"
+)
+const mobileCompanyParag = document.querySelector(".mobile-menu .company p")
+
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".mobile-menu-icon img")) {
+    mobileMenu.classList.toggle("invisible")
+  }
+
+  if (
+    e.target.matches(".mobile-menu .close-menu-icon img") ||
+    e.target.matches(".mobile-menu .dropdown-menu p") ||
+    e.target.matches(".mobile-menu .careers p") ||
+    e.target.matches(".mobile-menu .about p") ||
+    e.target.matches(".mobile-menu .registration p")
+  ) {
+    mobileMenu.classList.toggle("invisible")
+    mobileFeaturesMenu.classList.add("invisible")
+    mobileCompanyMenu.classList.add("invisible")
+    mobileFeaturesParag.classList.remove("black")
+    mobileFeaturesArrowUp.classList.remove("black")
+    mobileCompanyParag.classList.remove("black")
+    mobileCompanyArrowUp.classList.remove("black")
+    mobileFeaturesArrowUp.classList.add("invisible")
+    mobileCompanyArrowUp.classList.add("invisible")
+    mobileFeaturesArrowDown.classList.remove("invisible")
+    mobileCompanyArrowDown.classList.remove("invisible")
+    return
+  }
+
+  if (
+    e.target.matches(".mobile-menu .features p") ||
+    e.target.matches(".mobile-menu .features i")
+  ) {
+    mobileFeaturesMenu.classList.toggle("invisible")
+    mobileFeaturesArrowDown.classList.toggle("invisible")
+    mobileFeaturesArrowUp.classList.toggle("invisible")
+
+    mobileFeaturesParag.classList.toggle("black")
+    mobileFeaturesArrowUp.classList.toggle("black")
+  }
+
+  if (
+    e.target.matches(".mobile-menu .company p") ||
+    e.target.matches(".mobile-menu .company i")
+  ) {
+    mobileCompanyMenu.classList.toggle("invisible")
+    mobileCompanyArrowDown.classList.toggle("invisible")
+    mobileCompanyArrowUp.classList.toggle("invisible")
+
+    mobileCompanyParag.classList.toggle("black")
+    mobileCompanyArrowUp.classList.toggle("black")
   }
 })
